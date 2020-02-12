278b67f7f327ce7a0878d754e552fa9031811360
karenadam <kadam@tsf-452-wpa-0-228.epfl.ch>
2020-02-12 10:20:41 +0100

    Now uses one shot reconstruction algorithm

--- Code/Figure3.py
+++ Code/Figure3.py
@@ -6,10 +6,10 @@ def get_results(x_param, A, t, end_time, Omega, kappa, delta, b):
     end_index = int(len(t) * 4 / 5)
     num_channels = len(A)
     delta_t = t[1] - t[0]
-    tem_mult = timeEncoder(kappa, delta, b, n_channels=num_channels)
-    spikes = tem_mult.encode_mixed_precise(x_param, A, Omega, end_time)
-    rec_mult = tem_mult.decode_multi_signal_multi_channel_recursive(
-        spikes, A, t, Omega, delta_t, num_iterations=100
+    tem_mult = timeEncoder(kappa, delta, b, A)
+    spikes = tem_mult.encode_precise(x_param, Omega, end_time, tol=1e-10)
+    rec_mult = tem_mult.decode_mixed(
+        spikes, t, x_param[0].get_sinc_locs(), Omega, delta_t
     )
     x_0 = x_param[0].sample(t)
     x_1 = x_param[1].sample(t)
@@ -26,16 +26,16 @@ def create_signal(num_signals, t, delta_t, Omega, sinc_padding):
     x = np.zeros((num_signals, len(t)))
     x_param = []
     for n in range(num_signals):
-        x_param.append(bandlimitedSignal(t, delta_t, Omega, padding=sinc_padding))
-        x[n, :] = x_param[-1].sample(t)
-    return x, x_param
+        x_param.append(bandlimitedSignal(Omega))
+        x_param[-1].random(t, padding = sinc_padding)
+    return x_param
 
 
-def get_params_for_spike_rate(x_param, t, A, end_time, spike_rates):
+def get_params_for_spike_rate(x_param, t, A, end_time, num_spikes):
     n_signals = len(x_param)
     x_ints = np.zeros((n_signals, 1))
     for n in range(n_signals):
-        x_ints[n] = x_param[n].get_total_integral(t)
+        x_ints[n] = x_param[n].get_precise_integral(0, end_time)
     y_ints = np.array(A).dot(x_ints)
     n_channels = len(A)
     b = [1, 1, 1]
@@ -43,7 +43,7 @@ def get_params_for_spike_rate(x_param, t, A, end_time, spike_rates):
     delta = [1] * n_channels
     kappadelta = [2 * kappa[l] * delta[l] for l in range(n_channels)]
     for m in range(n_channels):
-        needed_integral = (spike_rates[m] * end_time + 1.5) * kappadelta[m]
+        needed_integral = (num_spikes[m] + 0.6) * kappadelta[m]
         b[m] = (needed_integral - y_ints[m, 0]) / end_time
     return b, kappa, delta
 
@@ -51,91 +51,93 @@ def get_params_for_spike_rate(x_param, t, A, end_time, spike_rates):
 def GetData():
     # Settings for x
     end_time = 20
-    sinc_padding = 3
+    sinc_padding = 2
     delta_t = 1e-4
     t = np.arange(0, end_time + delta_t, delta_t)
     Omega = np.pi
     seed = 0
     np.random.seed(int(seed))
     num_signals = 2
-    rOmega = Omega / np.pi
+    num_sincs = end_time - sinc_padding*2
+    total_deg_freedom = num_sincs*num_signals
 
     # Settings for time encoding machine
     num_channels = 3
     A = [[0.4, 1], [0.3, -0.2], [1, 0.7]]
-    spike_rate1 = 0.5
-    spike_rate2 = 0.75
-    spike_range = np.arange(0.1, 1.7, 0.1)
-    n_spikes_total = np.zeros_like(spike_range)
-    n_spikes_const = np.zeros_like(spike_range)
-    for n_s_r in range(len(spike_range)):
-        n_spikes_const[n_s_r] = round(
-            min(spike_rate1, rOmega)
-            + min(spike_rate2, rOmega)
-            + min(spike_range[n_s_r], rOmega),
-            2,
-        )
-        n_spikes_total[n_s_r] = round(spike_rate1 + spike_rate2 + spike_range[n_s_r], 2)
+
+    num_constraints_1 = 12
+    num_constraints_2 = 8
+    num_constraints_3_range = np.arange(2, 21, 1)
+    n_constraints_total = np.zeros_like(num_constraints_3_range)
+    n_constraints_const = np.zeros_like(num_constraints_3_range)
+    for n_s_r in range(len(num_constraints_3_range)):
+        n_constraints_const[n_s_r] = min(num_constraints_1, num_sincs)\
+            + min(num_constraints_2, num_sincs)\
+            + min(num_constraints_3_range[n_s_r], num_sincs)
+        n_constraints_total[n_s_r] = num_constraints_1+num_constraints_2+num_constraints_3_range[n_s_r]
 
     # Settings for Simulation
     num_trials = 100
-    results = np.zeros((num_trials, num_signals, len(spike_range)))
+    results = np.zeros((num_trials, num_signals, len(num_constraints_3_range)))
 
     for n_t in range(num_trials):
         print(n_t)
-        x, x_param = create_signal(num_signals, t, delta_t, Omega, sinc_padding)
-        for n_s_r in range(len(spike_range)):
+        x_param = create_signal(num_signals, t, delta_t, Omega, sinc_padding)
+        for n_s_r in range(len(num_constraints_3_range)):
             b, kappa, delta = get_params_for_spike_rate(
-                x_param, t, A, end_time, [spike_rate1, spike_rate2, spike_range[n_s_r]]
+                x_param, t, A, end_time, [num_constraints_1,num_constraints_2,num_constraints_3_range[n_s_r]]
             )
             res1, res2 = get_results(x_param, A, t, end_time, Omega, kappa, delta, b)
             results[n_t, 0, n_s_r] = res1
             results[n_t, 1, n_s_r] = res2
 
-    filename = "../Data/Figure3.pkl"
-    with open(filename, "wb") as f:  # Python 3: open(..., 'wb')
-        pickle.dump([n_spikes_total, n_spikes_const, results, rOmega], f)
+    data_filename = Data_Path+"Figure3.pkl"
+    with open(data_filename, "wb") as f:  # Python 3: open(..., 'wb')
+        pickle.dump([n_constraints_total, n_constraints_const, results, total_deg_freedom], f)
 
 
 def GenerateFigure():
 
-    data_filename = "../Data/Figure3.pkl"
+    data_filename = Data_Path+"Figure3.pkl"
+    figure_filename = Figure_Path+"Figure3.png"
+
     with open(data_filename, "rb") as f:  # Python 3: open(..., 'wb')
         obj = pickle.load(f, encoding="latin1")
 
-    n_spikes_total = obj[0]
-    n_spikes_const = obj[1]
+    n_constraints_total = obj[0]
+    n_constraints_const = obj[1]
     results = obj[2]
     num_signals = results.shape[1]
-    rOmega = obj[3]
+    total_deg_freedom = obj[3]
 
     plt.rc("text", usetex=True)
 
     clr = plt.rcParams["axes.prop_cycle"].by_key()["color"]
     plt.figure(figsize=(8, 3))
-    plt.plot(n_spikes_total, np.mean(results[:, 0, :], 0), label=r'$x^{(0)}(t)$')
-    plt.plot(n_spikes_total, np.mean(results[:, 1, :], 0), label=r'$x^{(1)}(t)$')
-    plt.xlabel('Total spiking rate')
+    plt.plot(n_constraints_total, np.mean(results[:, 0, :], 0), label=r'$x^{(0)}(t)$')
+    plt.plot(n_constraints_total, np.mean(results[:, 1, :], 0), label=r'$x^{(1)}(t)$')
+    plt.xlabel('Total number of constraints')
     plt.ylabel('Reconstruction error')
     plt.legend(loc = 'best')
     ax = plt.gca()
     ax.set_yscale("log")
     ax2 = ax.twiny()
     ax2.set_xlim(ax.get_xlim())
-    ax.set_xticks(n_spikes_total[0::2])
-    ax.set_xticklabels(n_spikes_total[0::2])
-    ax2.set_xticks(n_spikes_total[0::2])
-    ax2.set_xticklabels(n_spikes_const[0::2])
+    ax.set_xticks(n_constraints_total[0::2])
+    ax.set_xticklabels(n_constraints_total[0::2])
+    ax2.set_xticks(n_constraints_total[0::2])
+    ax2.set_xticklabels(n_constraints_const[0::2])
     ax = plt.gca()
     ax.set_yscale("log")
-    plt.axvline(num_signals * rOmega, color="red")
-    plt.xlabel('Constrained spiking rate')
+    ax2.axvline(total_deg_freedom, color="red")
+    plt.xlabel('Number of useful constraints')
     plt.ylabel('Reconstruction Error')
-    plt.savefig("../Figures/Figure3.png")
+    plt.tight_layout()
+    plt.savefig(figure_filename)
 
 
 if __name__ == "__main__":
-    data_filename = "../Data/Figure3.pkl"
+    data_filename = Data_Path+"Figure3.pkl"
 
     if not os.path.isfile(data_filename):
         GetData()
--- Code/header.py
+++ Code/header.py
@@ -8,6 +8,10 @@ sys.path.insert(0, os.path.split(os.path.realpath(__file__))[0] + "/../Multi-Cha
 from Time_Encoder import *
 from Signal import *
 
+
+Figure_Path = os.path.split(os.path.realpath(__file__))[0] + "/../Figures/"
+Data_Path = os.path.split(os.path.realpath(__file__))[0] + "/../Data/"
+
 graphical_import = True
 
 if graphical_import:
